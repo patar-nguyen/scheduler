2dba7f420c1861286c7adb753d222147a7f8a4a2
Patrick Nguyen <patar.nguyen@gmail.com>
2022-02-23 21:30:16 -0500

    Add The Schedule

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/DayListItem.scss";
+import DayListItem from "components/DayListItem.js";
+
+export default function DayList(props) {
+  return (
+    <ul>
+      {props.days.map(day => (
+        <DayListItem
+        key = {day.id}
+        name = {day.name}
+        spots = {day.spots}
+        selected = {day.name === props.value}
+        setDay = {props.onChange}
+        />
+      ))}
+    </ul>
+  );
+}
