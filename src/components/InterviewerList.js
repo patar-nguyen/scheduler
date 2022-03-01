2b0159fea0fc83f0719a4c82bb03299e416adb0b
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-01 17:05:14 -0500

    Adding tests

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,9 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -21,3 +21,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
